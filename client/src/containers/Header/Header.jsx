fb69cc17cf64db9bd8fc4bfe1821a0a1d40280f8
tdkunz <74592421+tdkunz@users.noreply.github.com>
2024-08-27 14:42:10 +0700

    trang chu + login data

--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -1,50 +1,102 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, Navigate, Link } from 'react-router-dom';
 import { Form, FormControl, Button } from 'react-bootstrap';
+import { useAuth } from '../../services/auth';
 
 import './Header.scss';
 
 const Header = () => {
-  return (
-    <header className='header-homepage'>
-        <div className='d-flex align-items-center text-center py-3 background-top-nav'>
-            <div className='header-top-nav'>
-                <div className='logo-banner-frame'>
-                    <NavLink to = '/'>
-                        <h2>Project</h2>
-                    </NavLink>
-                </div>
-                <div className='header-login-frame'>
-                    <Form className='login-form'>
-                        <FormControl type="text" placeholder="Username" className="mr-sm-2" />
-                        <FormControl type="password" placeholder="Password" className="mr-sm-2" />
-                        <Button variant="warning">Login</Button>
-                    </Form>
+    const { isLoggedIn, login, logout } = useAuth();
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // làm với api
+        // try {
+        //     const response = await axios.post('http://localhost:8080/api/auth/login', formData);
+        //     if (response.status === 200) {
+
+        //         // Muốn lưu gì trong local thì làm ở đây 
+        //         // localStorage.setItem('accessToken', response.data.accessToken);
+        //         // localStorage.setItem('employeeId', response.data.customerId);
+        //         localStorage.setItem('isLoggedIn', true);
+        //         navigate('/');
+        //         login();
+        //     } else {
+        //         const message = response.data.message || 'An error occurred while login';
+        //         alert(message);
+        //     }
+        // } catch (error) {
+        //     const message = error.response?.data?.message || 'An error occurred while login';
+        //     alert(message)
+
+        // }
+        console.log("Login data: ", formData)
+    };
+
+    return (
+        <header className='header-homepage'>
+            <div className='d-flex align-items-center text-center py-3 background-top-nav'>
+                <div className='header-top-nav'>
+                    <div className='logo-banner-frame'>
+                        <NavLink to = '/'>
+                            <h2>Project</h2>
+                        </NavLink>
+                    </div>
+                    <div className='header-login-frame'>
+                        <Form className='login-form' onSubmit={handleSubmit}>
+                            <FormControl type="text" 
+                                         name = 'username' 
+                                         placeholder="Username" 
+                                         className="mr-sm-2" 
+                                         value={formData.username}
+                                         onChange={handleChange}
+                                         required />
+                            <FormControl type="password" 
+                                         name = 'password'
+                                         placeholder="Password" 
+                                         className="mr-sm-2"
+                                         value={formData.password}
+                                         onChange={handleChange} 
+                                         required />
+                            <Button type='submit' variant="warning">Login</Button>
+                        </Form>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className='header-menu'>
-            <NavLink to = '/' className='child-content'>
-                Thông báo
-            </NavLink>
-            <NavLink to = '/leave' className='child-content'>
-                Nghỉ phép
-            </NavLink>
-            <NavLink to = '/update-time-sheet' className='child-content'>
-                Update Time-sheet
-            </NavLink>
-            <NavLink to = '/work-from-home' className='child-content'>
-                Work from home
-            </NavLink>
-            <NavLink to = '/approve' className='child-content'>
-                Approve
-            </NavLink>
-            <NavLink to = '/activities' className='child-content'>
-                Activities
-            </NavLink>
-        </div>
-    </header>
-  );
+            <div className='header-menu'>
+                <NavLink to = '/' className='child-content'>
+                    Thông báo
+                </NavLink>
+                <NavLink to = '/leave' className='child-content'>
+                    Nghỉ phép
+                </NavLink>
+                <NavLink to = '/update-time-sheet' className='child-content'>
+                    Update Time-sheet
+                </NavLink>
+                <NavLink to = '/work-from-home' className='child-content'>
+                    Work from home
+                </NavLink>
+                <NavLink to = '/approve' className='child-content'>
+                    Approve
+                </NavLink>
+                <NavLink to = '/activities' className='child-content'>
+                    Activities
+                </NavLink>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
--- client/src/containers/HomePages/HomePage.jsx
+++ client/src/containers/HomePages/HomePage.jsx
@@ -10,8 +10,71 @@ const HomePage = () => {
     <React.Fragment>
         <Header/>
         <section>
-          <div>
-            Thông báo section
+          <div className='content-frame'>
+            <div className='notification'>
+              <ul className='display-listing'>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Kế hoạch nghỉ lễ 2/9 </div>
+                    <span>27/08/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Danh sách các nhân viên tham gia hoạt động tốt tháng 07 </div>
+                    <span>02/08/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Điều chỉnh danh sách nhân viên của các phòng ban </div>
+                    <span>01/08/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Danh sách khen thưởng nhân viên xuất sắc quý II </div>
+                    <span>10/07/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Danh sách các nhân viên tham gia hoạt động tốt tháng 06 </div>
+                    <span>02/07/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Kế hoạch tổ chức du lịch </div>
+                    <span>25/06/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Tọa đàm chia sẻ kinh nghiệm quản lý dự án </div>
+                    <span>20/06/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Hoạt động mới: Chạy bộ </div>
+                    <span>01/06/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Hướng dẫn sử dụng trang web nội bộ mới của công ty </div>
+                    <span>15/05/2024</span>
+                  </div>
+                </li>
+                <li className='listing-item'>
+                  <div className='d-flex justify-content-start'>
+                    <div className='item-title'>Thông báo nâng cấp trang web nội bộ </div>
+                    <span>15/05/2024</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
           <RightSidebar/>
         </section>
