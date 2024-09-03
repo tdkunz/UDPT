f91d3987e15da63f32b8c5b84a28a851e63d9698
Minh <minh2209ak@gmail.com>
2024-09-03 09:51:22 +0700

    FIx api approve, add api checkin/checkout

--- server/request-service/src/main/java/com/example/requestservice/repository/RequestRepository.java
+++ server/request-service/src/main/java/com/example/requestservice/repository/RequestRepository.java
@@ -1,11 +1,13 @@
 package com.example.requestservice.repository;
 
-
 import com.example.requestservice.model.Request;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RequestRepository extends MongoRepository<Request, String> {
-    // Custom queries if needed
+    List<Request> findByStatus(String status);
+    List<Request> findByStatusNot(String status);
 }
